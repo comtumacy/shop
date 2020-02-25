4654dc6e4c5dad260ae41c579f3a8065ca492015
lyt <1014741581@qq.com>
2020-02-25 17:23:45 +0800

    20200225

--- dist/index/goodsbar/goodsbar.js
+++ dist/index/goodsbar/goodsbar.js
@@ -13,6 +13,22 @@ Component({
     },
     timeData: {
       "type": Object
+    },
+    Title: {
+      "type": String,
+      "value": "今日秒杀"
+    },
+    SecondTitle: {
+      "type": String,
+      "value": "每晚22点自动更新"
+    },
+    SecondTitleSign: {
+      "type": Boolean,
+      "value": true
+    },
+    more: {
+      "type": String,
+      "value": "更多"
     }
   },
   data: {
