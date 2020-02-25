eee8b24234f4484c30429a8d9385de405c891a18
lyt <1014741581@qq.com>
2020-02-25 18:13:28 +0800

    20200225

--- dist/index/goodsbar/goodsbar.js
+++ dist/index/goodsbar/goodsbar.js
@@ -1,7 +1,7 @@
 Component({
   externalClasses: ['sp-class'],
-  created() { },
-  attached(res) { },
+  created() {},
+  attached(res) {},
   ready() { },
   moved() { },
   detached() { },
@@ -22,6 +22,7 @@ Component({
       "type": String,
       "value": "每晚22点自动更新"
     },
+    // 开启小标题开关
     SecondTitleSign: {
       "type": Boolean,
       "value": true
@@ -29,9 +30,20 @@ Component({
     more: {
       "type": String,
       "value": "更多"
+    },
+    // 是否自动开始倒计时
+    autoStart: {
+      "type": Boolean,
+      "value": true
+    },
+    imgs: {
+      "type": ArrayBuffer,
+      value: ["../../../image/goodsbar/fruits/apple.png", "../../../image/goodsbar/fruits/banana.png", "../../../image/goodsbar/fruits/cherry.png", "../../../image/goodsbar/fruits/jujube.png"]
     }
   },
   data: {
+    // 开启毫秒计算
+    millisecond: true
   },
 
   methods: {
