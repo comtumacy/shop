7eff609a418246c3e05da8ae0b6a70df91274954
lyt <1014741581@qq.com>
2020-02-26 12:10:33 +0800

    20200226

--- dist/index/goodsbar/goodsbar.js
+++ dist/index/goodsbar/goodsbar.js
@@ -7,17 +7,21 @@ Component({
   detached() {},
   error() {},
   properties: {
+    // 时间设置
     time: {
       "type": Number,
       "value": 30 * 60 * 60 * 1000
     },
+    // 自定义时间构建对象
     timeData: {
       "type": Object
     },
+    // 大标题
     Title: {
       "type": String,
       "value": "今日秒杀"
     },
+    // 子标题
     SecondTitle: {
       "type": String,
       "value": "每晚22点自动更新"
@@ -27,6 +31,7 @@ Component({
       "type": Boolean,
       "value": false
     },
+    // “更多”文字设置
     more: {
       "type": String,
       "value": "更多"
@@ -36,18 +41,22 @@ Component({
       "type": Boolean,
       "value": true
     },
+    // 图片路径
     imgs: {
       "type": ArrayBuffer,
       "value": ["../../../image/goodsbar/fruits/apple.png", "../../../image/goodsbar/fruits/banana.png", "../../../image/goodsbar/fruits/cherry.png", "../../../image/goodsbar/fruits/jujube.png"]
     },
+    // 图片优惠文字设置
     fourDiscountText: {
       "type": ArrayBuffer,
       "value": ["立省4元", "立省6元", "立省14元", "立省10元"]
     },
+    // 商品标题
     TitleText: {
       "type": ArrayBuffer,
       "value": ["烟台红富士苹果", "都乐进口香蕉2kg装", "山东大樱桃智利车厘子", "福建牛奶青枣2kg一装"]
     },
+    // 优惠类型
     priceType: {
       "type": ArrayBuffer,
       "value": [{
@@ -64,10 +73,54 @@ Component({
         }
       ]
     },
+    // 选用优惠类型标志
     priceTypeSign: {
       "type": Number,
       "value": 0
-    }
+    },
+    // 现价
+    priceColLeftNew: {
+      "type": ArrayBuffer,
+      "value": [10, 23.3, 45.2, 30]
+    },
+    // 原价
+    priceColLeftOld: {
+      "type": ArrayBuffer,
+      "value": [10, 23.3, 45.2, 30]
+    },
+    // 单位
+    priceColLeftCompany: {
+      "type": ArrayBuffer,
+      "value": ["/份", "/箱", "/份", "/个"]
+    },
+
+    // 样式设置
+    // 大标题字体大小与颜色
+    TitleFontSize: {
+      "type": String,
+      "value": "35"
+    },
+    TitleColor: {
+      "type": String,
+      "value": "#000"
+    },
+    // 小标题外边距、字体大小、 字体颜色
+    goodsbarSecondTitleMarginLeft: {
+      "type": String,
+      "value": "20"
+    },
+    goodsbarSecondTitleMarginTop: {
+      "type": String,
+      "value": "10"
+    },
+    goodsbarSecondTitleFontSize: {
+      "type": String,
+      "value": "16"
+    },
+    goodsbarSecondTitleColor: {
+      "type": String,
+      "value": "#80848F"
+    },
   },
   data: {
     // 开启毫秒计算
