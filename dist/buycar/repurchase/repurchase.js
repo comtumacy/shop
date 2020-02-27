701e0f064810145728160a6eb45a72f076d9d282
lyt <1014741581@qq.com>
2020-02-27 12:00:50 +0800

    20200227

---
+++ dist/buycar/repurchase/repurchase.js
@@ -0,0 +1,84 @@
+Component({
+  externalClasses: ['sp-class'],
+
+  properties: {
+    repurchaseLeft: {
+      "type": String,
+      "value": "换购"
+    },
+    // 形容词，动词，数量
+    describe1: {
+      "type": String,
+      "value": "满"
+    },
+    describe2: {
+      "type": String,
+      "value": "差"
+    },
+    number1: {
+      "type": String,
+      "value": "3"
+    },
+    number2: {
+      "type": String,
+      "value": "1"
+    },
+    number3: {
+      "type": String,
+      "value": "1"
+    },
+    company1: {
+      "type": String,
+      "value": "件"
+    },
+    company2: {
+      "type": String,
+      "value": "件"
+    },
+    company3: {
+      "type": String,
+      "value": "件"
+    },
+    verb: {
+      "type": String,
+      "value": "换购"
+    },
+    // 右边按钮文字
+    repurchaseRightText: {
+      "type": String,
+      "value": "换购"
+    },
+    // 样式
+    repurchaseLeftFontSize: {
+      "type": String,
+      "value": "20"
+    },
+    repurchaseLeftPadingUD: {
+      "type": String,
+      "value": "6"
+    },
+    repurchaseLeftPadingLR: {
+      "type": String,
+      "value": "10"
+    },
+    repurchaseLeftBackgroundColor: {
+      "type": String,
+      "value": "#19be6b"
+    },
+    repurchaseLeftColor: {
+      "type": String,
+      "value": "#ffffff"
+    }
+  },
+  created() {},
+  attached() {},
+  ready() {},
+  detached() {},
+  moved() {},
+
+  data: {},
+
+  methods: {
+
+  }
+})
