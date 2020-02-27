593a8e23dea556b81ecdcb32b683a89ce614c887
lyt <1014741581@qq.com>
2020-02-27 10:53:15 +0800

    20200227

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -88,6 +88,63 @@ Component({
       "type": String,
       "value": ""
     },
+    // 图片链接
+    icon1: {
+      "type": String,
+      "value": "wap-home-o"
+    },
+    icon2: {
+      "type": String,
+      "value": "shopping-cart-o"
+    },
+    icon3: {
+      "type": String,
+      "value": "friends-o"
+    },
+    icon4: {
+      "type": String,
+      "value": "friends-o"
+    },
+    icon5: {
+      "type": String,
+      "value": "friends-o"
+    },
+    // 激活时图片链接
+    iconActive1: {
+      "type": String,
+      "value": "wap-home-o"
+    },
+    iconActive2: {
+      "type": String,
+      "value": "shopping-cart-o"
+    },
+    iconActive3: {
+      "type": String,
+      "value": "friends-o"
+    },
+    iconActive4: {
+      "type": String,
+      "value": "friends-o"
+    },
+    iconActive5: {
+      "type": String,
+      "value": "friends-o"
+    },
+    // 选中标签的颜色
+    activeColor: {
+      "type": String,
+      "value": "#1989fa"
+    },
+    // 未选中标签的颜色
+    inactiveColor: {
+      "type": String,
+      "value": "#7d7e80"
+    },
+    // 是否为 iPhoneX 留出底部安全距离
+    safeAreaInsetBottom: {
+      "type": Boolean,
+      "value": true
+    }
   },
   created() {
   },
